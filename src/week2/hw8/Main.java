030d78cdd49668937acb1ed2bf41287cc0ff2353
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-10-21 14:05:21 +0300

    Week 2 Homework 8

--- src/week2/hw8/Main.java
+++ src/week2/hw8/Main.java
@@ -7,7 +7,7 @@ import java.math.BigInteger;
  *
  * Факториал NN вычисляется как 1⋅2⋅...⋅N1⋅2⋅...⋅N.
  *
- * Поскольку это очень быстро растущая функция, то даже для небольших NN вместимости типов int и long очень скоро
+ * Поскольку это очень быстро растущая функция, то даже для небольших N вместимости типов int и long очень скоро
  * не хватит. Поэтому будем использовать BigInteger.
  */
 public class Main {
@@ -17,8 +17,7 @@ public class Main {
      * @param value positive number
      * @return factorial of <code>value</code>
      */
-    @SuppressWarnings("WeakerAccess")
-    public static BigInteger factorial(int value) {
+    static BigInteger factorial(int value) {
         BigInteger result = BigInteger.valueOf(1);
         for (int i = 1; i < value + 1; i++)
             result = result.multiply(BigInteger.valueOf(i));
