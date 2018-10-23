57d0bcac78dd104e9160e21d5bcbc580d5eb2e74
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-10-23 20:46:33 +0300

    Add missing docstring for Week 4 Homework 1

--- src/week4/hw1/Main.java
+++ src/week4/hw1/Main.java
@@ -1,5 +1,10 @@
 package week4.hw1;
 
+/**
+ * Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(), это метод при передаче
+ * отрицательного параметра должен бросать исключение java.lang.IllegalArgumentException с сообщением
+ * "Expected non-negative number, got ?", где вместо вопросика будет подставлено фактически переданное в метод число.
+ */
 public class Main {
     static double sqrt(double x) throws IllegalArgumentException {
         if (x < 0) {
